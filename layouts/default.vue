b8679624be348e2ac52a2620e1d0e881900805a6
nhut-minh-tran <nhutminhtran05@gmail.com>
2024-12-08 17:54:35 +0700

    Create project finance tracker

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container mx-auto max-w-6xl">
+        <AppHeader />
+        <main class="my-10">
+            <slot />
+        </main>
+    </div>
+</template>
+
+
+<script setup>
+import AppHeader from '~/components/app-header.vue';
+
+
+useHead({
+    link: [
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+            crossorigin: ''
+        }
+    ]
+})
+</script>
+
+<style>
+
+body{
+    font-family: 'Inter';
+}
+
+body{
+    @apply dark:bg-gray-900 bg-white;
+}
+
+</style>
---
+++ pages/settings/avatar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="mb-4">
+            <UFormGroup label="Current avatar" class="w-full" help="This would be blank by default">
+                <UAvatar :src="url" size="3xl" />
+            </UFormGroup>
+        </div>
+        <div class="mb-4">
+            <UFormGroup label="New avatar" class="w-full" name="avatar" help="After choosing an image click Save to actually upload the new avatar">
+                <UInput type="file" ref="fileInput" />
+            </UFormGroup>
+        </div>
+
+        <UButton type="submit" color="black" variant="solid" label="Save" :loading="uploading" @click="saveAvatar"/>
+    </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const { toastSuccess, toastError } = useAppToast()
+const { url } = useAvatarUrl()
+const uploading = ref(false)
+const fileInput = ref()
+
+const saveAvatar = async () => {
+    const file = fileInput.value.input.files[0]
+
+    if(!file){
+        toastError({title: 'Select a file to upload first'})
+        return
+    }
+
+    const fileExt = file.name.split('.').pop()
+    const fileName = `${new Date().getTime()}.${fileExt}`
+   
+    try{
+        uploading.value = true
+        const currentAvatarUrl = user.value.user_metadata?.avatar_url
+        const { error } = await supabase.storage.from('avatars')
+                .upload(fileName,file)
+        if(error) throw error
+        await supabase.auth.updateUser({
+            data: {
+                avatar_url: fileName
+            }
+        })
+        if(currentAvatarUrl){
+           const { error } = await supabase.storage.from('avatars').remove([currentAvatarUrl])
+           if(error) throw error
+        }
+        fileInput.value.input.value = null
+        toastSuccess({
+            title: 'Avatar uploaded'
+        })
+    }catch(error){
+        toastError({
+            title: 'Error uploading avatar',
+            description: error.message
+        })
+    }finally{
+        uploading.value = false
+    }
+}
+</script>
